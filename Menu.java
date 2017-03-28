0ac286311ffb4e0dbee7d79a7d0c8039a8992725
Emil Lindblom <emillind@kth.se>
2017-03-28 16:57:58 +0200

    Now works for all inputs.

--- Menu.java
+++ Menu.java
@@ -2,45 +2,60 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 public class Menu{
-Scanner scan;
-  public Menu(){
-    scan = new Scanner(System.in);
-  }
+	Scanner scan;
+	public Menu(){
+		scan = new Scanner(System.in);
+	}
 
-  private void writeMenu(){
-    System.out.println(1 + ": Sour");
-    System.out.println(2 + ": Grogg");
-    System.out.println(3 + ": Cocktail");
-    System.out.println(4 + ": Öl");
-    System.out.println(5 + ": Exit");
-    System.out.print(">");
-  }
+	private void writeMenu(){
+		System.out.println(1 + ": Sour");
+		System.out.println(2 + ": Grogg");
+		System.out.println(3 + ": Cocktail");
+		System.out.println(4 + ": Öl");
+		System.out.println(5 + ": Exit");
+		System.out.print(">");
+	}
 
-  public void getResponse(){
-    int response;
-    while(true){
-      writeMenu();
-      response = scan.nextInt();
-      if(response == 1){
-        Sour s = new Sour(new PartitionGenerator(), new Inventory());
-        System.out.println(s.getString());
-      }
-      else if(response == 2){
-        Grogg g = new Grogg(new PartitionGenerator(), new Inventory());
-        System.out.println(g.getString());
-      }
-      else if(response == 3){
-        Cocktail c = new Cocktail(new PartitionGenerator(), new Inventory());
-        System.out.println(c.getString());
-      }
-      else if(response == 4){
-        Beer b = new Beer(new PartitionGenerator(), new Inventory());
-        System.out.println(b.getString());
-      }
-      else if(response == 5){
-        break;
-      }
-    }
-    System.out.println("Barstoppad?");
-  }
+	public void getResponse(){
+		int response;
+		loop: while(true){
+			writeMenu();
+			response = getInt(scan.next());
+			switch (response) {
+				case 1:
+					Sour s = new Sour(new PartitionGenerator(), new Inventory());
+					System.out.println("Sour: " + s.getString());
+					break;
+				case 2:
+					Grogg g = new Grogg(new PartitionGenerator(), new Inventory());
+					System.out.println("Grogg: " + g.getString());
+					break;
+				case 3:
+					Cocktail c = new Cocktail(new PartitionGenerator(), new Inventory());
+					System.out.println("Cocktail: " + c.getString());
+					break;
+				case 4:
+					Beer b = new Beer(new PartitionGenerator(), new Inventory());
+					System.out.println("Beer: " + b.getString());
+					break;
+				case 5:
+					break loop;
+				default:
+					System.out.println("Är du full eller? Försök igen");
+			}
+			System.out.println("----------------------------------------");
+		}
+		System.out.println("Barstoppad?");
+	}
+
+	/**
+	 * Returns 0 if input String is not an int.
+	 */
+	private int getInt(String next) {
+		try {
+			return Integer.parseInt(next);
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	}
 }
